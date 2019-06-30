ba8e6fd2a969ad342fadd15f6f94e678a5461c6c
cartantino <c.carta4@campus.unimib.it>
2019-06-30 11:38:05 +0200

    added roll,pitch, and vector acc evaluation for each line of the dataset

---
+++ preprocessing.py
@@ -0,0 +1,55 @@
+import pandas as pd
+import csv
+import numpy as np
+from pprint import pprint
+# PREPROCESSING
+
+
+# Carica il dataset dal file csv. Si potrebbe migliorare specificando anche il tipo di ciascuna variabile
+def loadDataset():
+    filename = 'dataset.csv'
+    raw_data = open(filename, 'rt')
+    reader = csv.reader(raw_data, delimiter=';')
+    x = list(reader)
+    dataset = np.array(x[1:])
+    dataset = pd.DataFrame(data={
+                            'user':[dataset[i][0]       for i in range(len(dataset))],
+                            'gender':[dataset[i][1]     for i in range(len(dataset))],
+                            'age':[int(dataset[i][2])   for i in range(len(dataset))],
+                            'height':[float(dataset[i][3]) for i in range(len(dataset))],
+                            'weight':[float(dataset[i][4]) for i in range(len(dataset))],
+                            'bmi':[float(dataset[i][5]) for i in range(len(dataset))],
+                            'x1': [ int(dataset[i][6]) for i in range(len(dataset)) ],
+                            'y1': [ int(dataset[i][7]) for i in range(len(dataset)) ],
+                            'z1': [ int(dataset[i][8]) for i in range(len(dataset)) ],
+                            'x2': [ int(dataset[i][9]) for i in range(len(dataset)) ],
+                            'y2': [ int(dataset[i][10]) for i in range(len(dataset)) ],
+                            'z2': [ int(dataset[i][11]) for i in range(len(dataset)) ],
+                            'x3': [ int(dataset[i][12]) for i in range(len(dataset)) ],
+                            'y3': [ int(dataset[i][13]) for i in range(len(dataset)) ],
+                            'z3': [ int(dataset[i][14]) for i in range(len(dataset)) ],
+                            'x4': [ int(dataset[i][15]) for i in range(len(dataset)) ],
+                            'y4': [ int(dataset[i][16]) for i in range(len(dataset)) ],
+                            'z4': [ int(dataset[i][17]) for i in range(len(dataset)) ],
+                            'classes':[dataset[i][18]   for i in range(len(dataset))]})
+    return dataset
+
+
+def features_extraction(dataset):
+
+    for i in range(1,5):
+        dataset['roll'+str(i)] = pd.Series(180/np.pi*(np.arctan2(dataset['y'+str(i)], dataset['z'+str(i)])))
+        dataset['pitch'+str(i)] = pd.Series(180/np.pi*(np.arctan2(-dataset['x'+str(i)], np.sqrt(np.power(dataset['y'+str(i)], 2) + np.power(dataset['z'+str(i)], 2)))))
+        dataset['total_accel_sensor_'+str(i)] = pd.Series(np.sqrt(np.power(dataset['x'+str(i)], 2) + np.power(dataset['y'+str(i)], 2) + np.power(dataset['z'+str(i)], 2)))
+
+    return dataset
+
+
+if __name__ == '__main__':
+    #Loading of the dataset
+    dataset = loadDataset()
+    #Evaluation of roll pitch and acceleration vector for any point at any time
+    dataset = features_extraction(dataset)
+
+
+    
