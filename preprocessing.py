d0096baef030eaa7e7b35eb3b8c49e687fd57a24
Hamza <hamza.27.96@icloud.com>
2019-06-30 23:47:31 +0200

    ended feature extraction. Started feature selection, probably errors in discretizing data

--- preprocessing.py
+++ preprocessing.py
@@ -6,9 +6,10 @@ import warnings ## Remove all warning
 warnings.filterwarnings('ignore')
 # PREPROCESSING
 
+############# FEATURE EXTRACTION ###################################################################################################
 
-# Carica il dataset dal file csv. Si potrebbe migliorare specificando anche il tipo di ciascuna variabile
-def loadDataset():
+# load dataset.csv
+def load_dataset():
     filename = 'dataset.csv'
     raw_data = open(filename, 'rt')
     reader = csv.reader(raw_data, delimiter=';')
@@ -36,16 +37,14 @@ def loadDataset():
                             'classes':[dataset[i][18]   for i in range(len(dataset))]})
     return dataset
 
-
 def features_extraction(dataset):
-
     for i in range(1,5):
         dataset['roll'+str(i)] = pd.Series(180/np.pi*(np.arctan2(dataset['y'+str(i)], dataset['z'+str(i)])))
         dataset['pitch'+str(i)] = pd.Series(180/np.pi*(np.arctan2(-dataset['x'+str(i)], np.sqrt(np.power(dataset['y'+str(i)], 2) + np.power(dataset['z'+str(i)], 2)))))
         dataset['total_accel_sensor_'+str(i)] = pd.Series(np.sqrt(np.power(dataset['x'+str(i)], 2) + np.power(dataset['y'+str(i)], 2) + np.power(dataset['z'+str(i)], 2)))
     return dataset
 
-
+# split dataset
 def sample_dataset(dataset_feature):
     classes = ['sittingdown','standingup','walking','standing','sitting']
     subjects = ['katia','debora','wallace','jose_carlos']
@@ -54,10 +53,10 @@ def sample_dataset(dataset_feature):
         for name in subjects:
             dataset_class_name = dataset_class[dataset_feature.user == name]
             dataset_class_name.drop(columns=['x1','y1','z1','x2','y2','z2','x3','y3','z3','x4','y4','z4'], axis=1, inplace=True)
-            dataset_class_name.to_csv('csv/' + clas + '_' + name + '_dataset.csv', sep = ';', index=False)
+            #dataset_class_name.to_csv('csv/' + clas + '_' + name + '_dataset.csv', sep = ';', index=False)
             sample_split(dataset_class_name,clas,name)
 
-
+# split dataset in relation of the class predicted and the user
 def sample_split(sample,clas,name):
     sample.index = [i for i in range(len(sample)) ]
     l = len(sample)/8
@@ -72,6 +71,7 @@ def sample_split(sample,clas,name):
         # appending to the empty data frame the new row
         new_sample = variance_evaluation(window)
         new_sample = split_classes(new_sample)
+        new_sample = acceleration_mean_stdev(new_sample)
 
         user = new_sample['user']
         gender = new_sample['gender']
@@ -87,7 +87,6 @@ def sample_split(sample,clas,name):
         pitch3 = new_sample['pitch3']
         roll4 = new_sample['roll4']
         pitch4 = new_sample['pitch4']
-        #classes = new_sample['classes']
         total_accel_sensor_1 = new_sample['total_accel_sensor_1']
         total_accel_sensor_2 = new_sample['total_accel_sensor_2']
         total_accel_sensor_3 = new_sample['total_accel_sensor_3']
@@ -97,14 +96,14 @@ def sample_split(sample,clas,name):
         walking = new_sample['walking']
         standing = new_sample['standing']
         sitting = new_sample['sitting']
+        acceleration_mean = new_sample['acceleration_mean']
+        acceleration_stdev = new_sample['acceleration_stdev']
 
         with open('data_sampled.csv','a') as csvFile:
-            row = [user, gender, age, height, weight, bmi, roll1, pitch1, roll2, pitch2, roll3, pitch3, roll4, pitch4, total_accel_sensor_1, total_accel_sensor_2, total_accel_sensor_3, total_accel_sensor_4, sittingdown, standingup, walking, standing, sitting]
+            row = [user, gender, age, height, weight, bmi, roll1, pitch1, roll2, pitch2, roll3, pitch3, roll4, pitch4, total_accel_sensor_1, total_accel_sensor_2, total_accel_sensor_3, total_accel_sensor_4, sittingdown, standingup, walking, standing, sitting, acceleration_mean, acceleration_stdev]
             writer = csv.writer(csvFile)
             writer.writerow(row)
         csvFile.close()
-
-
 
 # evaluate variance of the eight rows in input
 def variance_evaluation(subset):
@@ -119,6 +118,7 @@ def variance_evaluation(subset):
 
     return new_row
 
+# split the column classes in 5 columns( sittingdown, standingup, walking, standing, sitting )
 def split_classes(sample):
     classes = ['sittingdown', 'standingup', 'walking', 'standing', 'sitting']
     classes_ = []
@@ -133,12 +133,84 @@ def split_classes(sample):
 
     return sample
 
+# calculate acceleration mean and standard deviation
+def acceleration_mean_stdev(sample):
+    sample['acceleration_mean'] = np.mean([sample['total_accel_sensor_1'],sample['total_accel_sensor_2'],sample['total_accel_sensor_3'],sample['total_accel_sensor_4']])
+    sample['acceleration_stdev'] = np.std([sample['total_accel_sensor_1'],sample['total_accel_sensor_2'],sample['total_accel_sensor_3'],sample['total_accel_sensor_4']])
+
+    return sample
+
+####################################################################################################################################
+
+############# FEATURE SELECTION #################################################################################################
+
+# load data_sampled.csv
+def load_data_sampled():
+    filename = 'data_sampled.csv'
+    raw_data = open(filename, 'rt')
+    reader = csv.reader(raw_data, delimiter=',')
+    x = list(reader)
+    dataset = np.array(x[1:])
+    dataset = pd.DataFrame(data={
+                            'user':[dataset[i][0]       for i in range(len(dataset))],
+                            'gender':[dataset[i][1]     for i in range(len(dataset))],
+                            'age':[int(dataset[i][2])   for i in range(len(dataset))],
+                            'height':[float(dataset[i][3]) for i in range(len(dataset))],
+                            'weight':[float(dataset[i][4]) for i in range(len(dataset))],
+                            'bmi':[float(dataset[i][5]) for i in range(len(dataset))],
+                            'roll1': [ float(dataset[i][6]) for i in range(len(dataset)) ],
+                            'pitch1': [ float(dataset[i][7]) for i in range(len(dataset)) ],
+                            'roll2': [ float(dataset[i][8]) for i in range(len(dataset)) ],
+                            'pitch2': [ float(dataset[i][9]) for i in range(len(dataset)) ],
+                            'roll3': [ float(dataset[i][10]) for i in range(len(dataset)) ],
+                            'pitch3': [ float(dataset[i][11]) for i in range(len(dataset)) ],
+                            'roll4': [ float(dataset[i][12]) for i in range(len(dataset)) ],
+                            'pitch4': [ float(dataset[i][13]) for i in range(len(dataset)) ],
+                            'total_accel_sensor_1': [ float(dataset[i][14]) for i in range(len(dataset)) ],
+                            'total_accel_sensor_2': [ float(dataset[i][15]) for i in range(len(dataset)) ],
+                            'total_accel_sensor_3': [ float(dataset[i][16]) for i in range(len(dataset)) ],
+                            'total_accel_sensor_4': [ float(dataset[i][17]) for i in range(len(dataset)) ],
+                            'sittingdown':[int(dataset[i][18])   for i in range(len(dataset))],
+                            'standingup':[int(dataset[i][19])   for i in range(len(dataset))],
+                            'walking':[int(dataset[i][20])   for i in range(len(dataset))],
+                            'standing':[int(dataset[i][22])   for i in range(len(dataset))],
+                            'sitting':[int(dataset[i][22])   for i in range(len(dataset))],
+                            'acceleration_mean':[float(dataset[i][23])   for i in range(len(dataset))],
+                            'acceleration_stdev':[float(dataset[i][24])   for i in range(len(dataset))]})
+    return dataset
+
+def feature_selection():
+    data = load_data_sampled()
+
+    data = discretize(data)
+
+    data.drop(columns=['user','gender','age','height','weight','bmi','total_accel_sensor_1','total_accel_sensor_3','total_accel_sensor_2_discretized','total_accel_sensor_3_discretized','total_accel_sensor_4','roll4','pitch4'], axis=1, inplace=True)
+
+    data.to_csv('data_discrete.csv', sep = ',', index=False)
+
+
+# discretize the module of acceleration of each accelerometer
+def discretize(data):
+    n=10
+    data['total_accel_sensor_1_discretized'] = pd.Series(pd.cut(x=data['total_accel_sensor_1'], bins=n, labels=list(range(n))))
+    data['total_accel_sensor_2_discretized'] = pd.Series(pd.cut(x=data['total_accel_sensor_2'], bins=n, labels=list(range(n))))
+    data['total_accel_sensor_3_discretized'] = pd.Series(pd.cut(x=data['total_accel_sensor_3'], bins=n, labels=list(range(n))))
+    data['total_accel_sensor_4_discretized'] = pd.Series(pd.cut(x=data['total_accel_sensor_4'], bins=n, labels=list(range(n))))
+    data['pitch1'] = pd.Series(pd.cut(x=data['pitch1'], bins=n, labels=list(range(n))))
+    data['roll1'] = pd.Series(pd.cut(x=data['roll1'], bins=n, labels=list(range(n))))
+    return data
+
+####################################################################################################################################
 if __name__ == '__main__':
+    #FEATURE EXTRACTION
     #Loading of the dataset
-    dataset = loadDataset()
+    #dataset = load_dataset()
     #Evaluation of roll pitch and acceleration vector for any point at any time
-    dataset = features_extraction(dataset)
+    #dataset = features_extraction(dataset)
     #Save data into a csv
-    dataset.to_csv('csv/measure_dataset.csv', sep = ';', index=False)
+    #dataset.to_csv('csv/measure_dataset.csv', sep = ';', index=False)
     #sample dataset by class
-    sample_dataset(dataset)
+    #sample_dataset(dataset)
+
+    #FEATURE SELECTION
+    feature_selection()
